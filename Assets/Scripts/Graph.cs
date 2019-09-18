8c32db69aae4aa1bc50f2cf260e73e7201dce30d
josh <48034855+JDJoffe@users.noreply.github.com>
2019-09-18 12:14:32 +1000

    initial

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Graph : MonoBehaviour
+{
+    public Transform pointPrefab;
+    [Range(10, 100)]
+    // public List<Transform> pointPrefabs;
+    public int resolution = 10;
+    public Transform[] points;
+    private void Awake()
+    {
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position = Vector3.zero;
+        Quaternion rotation = Quaternion.Euler(1, 1, 1);
+        points = new Transform[resolution];
+        // increments i then returns it
+        // ++i;
+        // returns i then increments it
+        // i++;
+        for (int i = 0; i < points.Length; i++)
+        {
+            // local transform = new instantiated prefab
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            position.y = position.x * position.x;
+            position.z = (position.y + step) * (position.x + step);
+            rotation.x = i * step;
+            rotation.y = i * step;
+            rotation.z = i * step;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.localRotation = rotation;
+            point.SetParent(transform, false);
+            points[i] = point;
+        }
+    }
+    private void Update()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            Quaternion rotation = point.localRotation;
+            // X
+            // position.x = Mathf.Sin(Mathf.PI * (position.z + Time.time)) ; // Sin
+            // position.x = Mathf.Cos(Mathf.PI * (position.z + Time.time)) ; // Cos
+            // position.x = Mathf.Tan(Mathf.PI * (position.z + Time.time)) ; // Tan
+            // Y
+
+            // position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time)); // Sin 
+            // position.y = Mathf.Cos(Mathf.PI * (position.x + Time.time)); // Cos 
+            // position.y = Mathf.Tan(Mathf.PI * (position.x + Time.time)); // Tan 
+
+            // Z
+            // position.z = Mathf.Sin(Mathf.PI * (position.y + Time.time)); // Sin
+            // position.z = Mathf.Cos(Mathf.PI * (position.y + Time.time)); // Cos
+            // position.z = Mathf.Tan(Mathf.PI * (position.y + Time.time)); // Tan
+
+            rotation.x = i * position.x + Time.time;
+            rotation.y = i * position.y + Time.time;
+            rotation.z = i * position.z + Time.time;
+            point.localPosition = position;
+            point.localRotation = rotation;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Shader.shader
@@ -0,0 +1,58 @@
+﻿Shader "Custom/Shader"
+{
+    Properties
+    {
+		//_MainTex("Main Texture", 2D) = "white" {}
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+       // sampler2D _MainTex;
+
+        struct Input
+        {
+            float3 worldPos;
+			//float2 uv_MainTex;
+        };
+
+        half _Glossiness;
+        half _Metallic;
+		float2 modulo(float2 divident, float2 divisor)
+		{
+			float2 positiveDivident = divident % divisor + divisor;
+			return positiveDivident % divisor;
+		}
+        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+        // #pragma instancing_options assumeuniformscaling
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            // Albedo comes from a texture tinted by color
+			o.Albedo.rg = modulo(float2(IN.worldPos.x,IN.worldPos.y), IN.worldPos.xy * 0.5 + 0.5);
+		//	o.Albedo.rg = Sin(float2(IN.worldPos.x,IN.worldPos.y), IN.worldPos.xy * 0.5 + 0.5);
+		//	modf(Sin(float2(IN.worldPos.x,IN.worldPos.y), IN.worldPos.xy * 0.5 + 0.5));
+            // Metallic and smoothness come from slider variables
+			//o.Albedo.rgb = fmod(IN.worldPos.x*10, 1) < 0 ? 0 : tex2D(_MainTex, IN.uv_MainTex);
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = 1;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
